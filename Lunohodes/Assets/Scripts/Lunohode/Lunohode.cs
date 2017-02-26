9f15c92a6b3e4c0883631775c37f8cefec721427
craus <craus902@gmail.com>
2017-02-26 19:50:56 +0400

    moving part

---
+++ Lunohodes/Assets/Scripts/Lunohode/Lunohode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lunohode : MonoBehaviour {
+    public Cell cell;
+    public float direction;
+
+    public List<Command> commands = new List<Command>();
+
+    public void Update() {
+        if (Input.GetAxis("Horizontal") > 0) {
+            Debug.LogFormat("Hor > 0");
+        }
+    }
+
+    public void MoveForward() {
+
+    }
+}
