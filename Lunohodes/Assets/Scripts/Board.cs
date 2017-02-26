a4db7759ef92c83c0d4d272ab803cf7b96a69a4a
craus <craus902@gmail.com>
2017-02-26 17:43:14 +0400

    board

---
+++ Lunohodes/Assets/Scripts/Board.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+    public int n = 8;
+    public float size = 10;
+    public Cell cellSample;
+    public Material white;
+    public Material black;
+
+    [ContextMenu("Create")]
+    void Create() {
+        transform.Children().ForEach(t => DestroyImmediate(t.gameObject));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                var cell = Instantiate(cellSample);
+                cell.GetComponent<MeshRenderer>().sharedMaterial = (i + j) % 2 == 0 ? white : black;
+                cell.transform.position = new Vector3(size * i, 0, size*j);
+                cell.transform.SetParent(transform);
+            }
+        }
+    }
+}
