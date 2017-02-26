e35ad9f1afa381582fcb54c608c7cbe1571c11d4
craus <craus902@gmail.com>
2017-02-26 18:01:38 +0400

    details and lunohode models

---
+++ Lunohodes/Assets/Scripts/PrefabFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabFactory : MonoBehaviour {
+    void Start() {
+        Destroy(gameObject);
+    }
+}
